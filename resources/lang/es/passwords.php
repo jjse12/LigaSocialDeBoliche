e32f9c9e01a0ae95d5f7ad41a15f1497b776f565
jjse12 <jjse127@gmail.com>
2018-08-29 18:59:06 -0600

    Add basic components
    Add models, migrations, controllers.
    Include laravel's auth plugin.

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe contener al menos 6 caracteres y coincidir con la confirmación.',
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado un correo electrónico con un enlace para restablecer tu contraseña!',
+    'token' => 'Este código para restablecer contraseña es invalido.',
+    'user' => "No existe un usuario registrado con ese correo electrónico.",
+
+];
